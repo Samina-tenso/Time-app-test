fb3bb332c01de3099f9884012c292b5ef583b328
Samina-tenso <tensammi@gmail.com>
2022-11-08 14:57:26 +0100

    fixed styling root

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom"
 export function Root() {
     return (
         <div>
-            <ul className="fixed bottom-0  z-20 bg-slate-900 inset-x-0 flex text-center justify-center " >
-                <NavLink className={({ isActive }) => isActive ? "bg-slate-600" : null} to={`/timer`} href="#"> <li className=" p-4 hover:bg-slate-600" > Timer</li></NavLink>
-                <NavLink className={({ isActive }) => isActive ? "bg-slate-600" : null} to={`/calender`} href="#"><li className=" p-4 hover:bg-slate-600"> Calender</li></NavLink>
-                <NavLink className={({ isActive }) => isActive ? "bg-slate-600" : null} to={`/projects`} href="#"> <li className="  p-4 hover:bg-slate-600"> Projects</li></NavLink>
+            <ul className="fixed bottom-0 z-20 bg-slate-900s inset-x-0 flex text-center justify-center " >
+                <NavLink className={({ isActive }) => isActive ? "bg-slate-600" : null} to={`/timer`} href="#"> <li className="p-4 hover:bg-slate-600" > Timer</li></NavLink>
+                <NavLink className={({ isActive }) => isActive ? "bg-slate-600" : null} to={`/calender`} href="#"><li className="p-4 hover:bg-slate-600"> Calender</li></NavLink>
+                <NavLink className={({ isActive }) => isActive ? "bg-slate-600" : null} to={`/projects`} href="#"> <li className="p-4 hover:bg-slate-600"> Projects</li></NavLink>
             </ul >
             <Outlet />
         </div >
